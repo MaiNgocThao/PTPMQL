25e46902164d0f459a812e9b7f4b66b37477e4fd
MaiNgocThao <Thaofc100@gmail.com>
2026-01-20 16:06:54 +0700

    CreateProject

---
+++ DemoMvcProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DemoMvcProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
